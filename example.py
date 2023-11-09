9eea8cef6178756a39c979c2ea5ff055afc15a6f
kinsDev <kinsleygitonga@gmail.com>
2023-11-09 08:09:55 +0300

    OOP syntax practice

--- example.py
+++ example.py
@@ -2,10 +2,17 @@ from shirt import Shirt
 
 new_shirt  = Shirt('red','M','long sleeved',45)
 shirt_two  = Shirt('orange','S','short sleeved',30)
+
 print(new_shirt.price)
 print(new_shirt.color)
+
 shirt_two.change_price(45)
 print(shirt_two.price)
+
+
 new_shirt.color = 'yellow'
-new_shirt.sze = 'L'
+new_shirt.size = 'L'
 new_shirt.price = 38
+print(new_shirt.color)
+print(new_shirt.size)
+print(new_shirt.price)

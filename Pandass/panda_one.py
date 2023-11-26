6fbd694786616ee69e02810186e79431027547b4
kinsDev <kinsleygitonga@gmail.com>
2023-11-26 13:50:32 +0300

    pandas testing

---
+++ Pandass/panda_one.py
@@ -0,0 +1,8 @@
+# We import Pandas as pd into Python
+import pandas as pd
+
+# We create a Pandas Series that stores a grocery list
+groceries = pd.Series(data=[30, 6, 'Yes', 'No'], index=['eggs', 'apples', 'milk', 'bread'])
+
+# We display the Groceries Pandas Series
+groceries
